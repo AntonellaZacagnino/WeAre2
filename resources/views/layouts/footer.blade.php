e232fd642b03156b6d3b04af8f8049e435052ce6
Unknown <antonellazacagnino@gmail.com>
2019-02-21 19:17:29 -0300

    migracion del trabajo a Laravel

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,13 @@
+<footer> <ul class="nav footer">
+  <li class="nav-item">
+    <a class="nav-link active" href="#">Comunidad</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Políticas de Seguridad</a>
+  </li>
+
+  <li class="nav-item">
+    <a class="nav-link" href="#">Contáctanos</a>
+  </li>
+  <li class="nav-item nav-link" style="color:white;">Digital House © 2019</li>
+</ul></footer>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('titulo')
+We Are - Home
+@endsection
+
+@section('content')
+
+          <link rel="stylesheet" href="/public/css/app.css">
+          <div class="caja">
+            <br>
+            <br>
+            <h3><strong>Bienvenid@ a We Are!</strong></h3>
+            <br>
+            <p class="texto-inicio">Es una plataforma de encuentro, un escenario donde te da la oportunidad de mostrar lo mejor de vos. Ayudamos a resaltar tu talento y que desde otro lado, te puedan localizar.
+Somos un facilitador de búsqueda, nuestro objetivo es fomentar la autogestión reduciendo los tiempos y costos. Unite y creá la red de contactos con más trayecto de latinoamérica.
+<br>
+Hacé tu profesión, un arte. Creá tu perfil, demostrá lo que te hace único. Encontrá lo que buscás.
+<br>
+Somos WeAre. Bienvenido!
+            </p>
+          </div>
+        </div>
+          <img class="caja" src="images/foto.png" alt="Card image  cap">
+      </div>
+
+@endsection
+
+@extends('layouts.footer')
